22cae387b5493e31d71679d5dc20645b49b83f59
Muhammad Salah <muhammad.salah1@ibm.com>
2018-03-19 13:54:57 +0200

    Fixing the convention naming to suite our style

--- security/CAInteraction/enrollAdmin.js
+++ security/CAInteraction/enrollAdmin.js
@@ -8,68 +8,68 @@
  * Enroll the admin user
  */
 
-var Fabric_Client = require('fabric-client');
-var Fabric_CA_Client = require('fabric-ca-client');
+var FabricClient = require('fabric-client');
+var FabricCaClient = require('fabric-ca-client');
 
 var path = require('path');
 var util = require('util');
 var os = require('os');
 
 //
-var fabric_client = new Fabric_Client();
-var fabric_ca_client = null;
-var admin_user = null;
-var member_user = null;
-var store_path = path.join(__dirname, 'hfc-key-store');
-console.log(' Store path:'+store_path);
+var fabricClient = new FabricClient();
+var fabricCaClient = null;
+var adminUser = null;
+var memberUser = null;
+var storePath = path.join(__dirname, 'hfc-key-store');
+console.log(' Store path:'+storePath);
 
 // create the key value store as defined in the fabric-client/config/default.json 'key-value-store' setting
-Fabric_Client.newDefaultKeyValueStore({ path: store_path
-}).then((state_store) => {
+FabricClient.newDefaultKeyValueStore({ path: storePath
+}).then((stateStore) => {
     // assign the store to the fabric client
-    fabric_client.setStateStore(state_store);
-    var crypto_suite = Fabric_Client.newCryptoSuite();
-    // use the same location for the state store (where the users' certificate are kept)
+    fabricClient.setStateStore(stateStore);
+    var cryptoSuite = Fabric_Client.newCryptoSuite();
+    // use the same location for Ce state store (where the users' certificate are kept)
     // and the crypto store (where the users' keys are kept)
-    var crypto_store = Fabric_Client.newCryptoKeyStore({path: store_path});
-    crypto_suite.setCryptoKeyStore(crypto_store);
-    fabric_client.setCryptoSuite(crypto_suite);
+    var cryptoStore = Fabric_Client.newCryptoKeyStore({path: storePath});
+    cryptoSuite.setCryptoKeyStore(cryptoStore);
+    fabricClient.setCryptoSuite(cryptoSuite);
     var	tlsOptions = {
     	trustedRoots: [],
     	verify: false
     };
     // be sure to change the http to https when the CA is running TLS enabled
-    fabric_ca_client = new Fabric_CA_Client('http://localhost:7054', tlsOptions , 'ca.example.com', crypto_suite);
+    fabricCaClient = new FabricCaClient('http://localhost:7054', tlsOptions , 'ca.example.com', cryptoSuite);
 
     // first check to see if the admin is already enrolled
     return fabric_client.getUserContext('admin', true);
-}).then((user_from_store) => {
-    if (user_from_store && user_from_store.isEnrolled()) {
+}).then((userFromStore) => {
+    if (userFromStore && userFromStore.isEnrolled()) {
         console.log('Successfully loaded admin from persistence');
-        admin_user = user_from_store;
+        adminUser = userFromStore;
         return null;
     } else {
         // need to enroll it with CA server
-        return fabric_ca_client.enroll({
+        return fabricCaClient.enroll({
           enrollmentID: 'admin',
           enrollmentSecret: 'adminpw'
         }).then((enrollment) => {
           console.log('Successfully enrolled admin user "admin"');
-          return fabric_client.createUser(
+          return fabricClient.createUser(
               {username: 'admin',
                   mspid: 'Org1MSP',
                   cryptoContent: { privateKeyPEM: enrollment.key.toBytes(), signedCertPEM: enrollment.certificate }
               });
         }).then((user) => {
-          admin_user = user;
-          return fabric_client.setUserContext(admin_user);
+          adminUser = user;
+          return fabric_client.setUserContext(adminUser);
         }).catch((err) => {
           console.error('Failed to enroll and persist admin. Error: ' + err.stack ? err.stack : err);
           throw new Error('Failed to enroll admin');
         });
     }
 }).then(() => {
-    console.log('Assigned the admin user to the fabric client ::' + admin_user.toString());
+    console.log('Assigned the admin user to the fabric client ::' + adminUser.toString());
 }).catch((err) => {
     console.error('Failed to enroll admin: ' + err);
 });
--- security/CAInteraction/registerUser.js
+++ security/CAInteraction/registerUser.js
@@ -8,57 +8,57 @@
  * Register and Enroll a user
  */
 
-var Fabric_Client = require('fabric-client');
-var Fabric_CA_Client = require('fabric-ca-client');
+var FabricClient = require('fabric-client');
+var FabricCAClient = require('fabric-ca-client');
 
 var path = require('path');
 var util = require('util');
 var os = require('os');
 
 //
-var fabric_client = new Fabric_Client();
-var fabric_ca_client = null;
-var admin_user = null;
-var member_user = null;
-var store_path = path.join(__dirname, 'hfc-key-store');
-console.log(' Store path:'+store_path);
+var fabricClient = new FabricClient();
+var fabricCaClient = null;
+var adminUser = null;
+var memberUser = null;
+var storePath = path.join(__dirname, 'hfc-key-store');
+console.log(' Store path:'+storePath);
 
 // create the key value store as defined in the fabric-client/config/default.json 'key-value-store' setting
-Fabric_Client.newDefaultKeyValueStore({ path: store_path
-}).then((state_store) => {
+FabricClient.newDefaultKeyValueStore({ path: storePath
+}).then((stateStore) => {
     // assign the store to the fabric client
-    fabric_client.setStateStore(state_store);
-    var crypto_suite = Fabric_Client.newCryptoSuite();
+    fabricClient.setStateStore(stateStore);
+    var cryptoSuite = FabricClient.newCryptoSuite();
     // use the same location for the state store (where the users' certificate are kept)
     // and the crypto store (where the users' keys are kept)
-    var crypto_store = Fabric_Client.newCryptoKeyStore({path: store_path});
-    crypto_suite.setCryptoKeyStore(crypto_store);
-    fabric_client.setCryptoSuite(crypto_suite);
+    var cryptoStore = FabricClient.newCryptoKeyStore({path: storePath});
+    cryptoSuite.setCryptoKeyStore(cryptoStore);
+    fabricClient.setCryptoSuite(cryptoSuite);
     var	tlsOptions = {
     	trustedRoots: [],
     	verify: false
     };
     // be sure to change the http to https when the CA is running TLS enabled
-    fabric_ca_client = new Fabric_CA_Client('http://localhost:7054', null , '', crypto_suite);
+    fabricCaClient = new FabricCAClient('http://localhost:7054', null , '', cryptoSuite);
 
     // first check to see if the admin is already enrolled
-    return fabric_client.getUserContext('admin', true);
-}).then((user_from_store) => {
-    if (user_from_store && user_from_store.isEnrolled()) {
+    return fabricClient.getUserContext('admin', true);
+}).then((userFromStore) => {
+    if (userFromStore && userFromStore.isEnrolled()) {
         console.log('Successfully loaded admin from persistence');
-        admin_user = user_from_store;
+        adminUser = userFromStore;
     } else {
         throw new Error('Failed to get admin.... run enrollAdmin.js');
     }
 
     // at this point we should have the admin user
     // first need to register the user with the CA server
-    return fabric_ca_client.register({enrollmentID: 'user1', affiliation: 'org1.department1'}, admin_user);
+    return FabricCAClient.register({enrollmentID: 'user1', affiliation: 'org1.department1'}, adminUser);
 }).then((secret) => {
     // next we need to enroll the user with CA server
     console.log('Successfully registered user1 - secret:'+ secret);
 
-    return fabric_ca_client.enroll({enrollmentID: 'user1', enrollmentSecret: secret});
+    return FabricCAClient.enroll({enrollmentID: 'user1', enrollmentSecret: secret});
 }).then((enrollment) => {
   console.log('Successfully enrolled member user "user1" ');
   return fabric_client.createUser(
@@ -67,9 +67,9 @@ Fabric_Client.newDefaultKeyValueStore({ path: store_path
      cryptoContent: { privateKeyPEM: enrollment.key.toBytes(), signedCertPEM: enrollment.certificate }
      });
 }).then((user) => {
-     member_user = user;
+     memberUser = user;
 
-     return fabric_client.setUserContext(member_user);
+     return fabricClient.setUserContext(memberUser);
 }).then(()=>{
      console.log('User1 was successfully registered and enrolled and is ready to intreact with the fabric network');
 
@@ -77,6 +77,6 @@ Fabric_Client.newDefaultKeyValueStore({ path: store_path
     console.error('Failed to register: ' + err);
 	if(err.toString().indexOf('Authorization') > -1) {
 		console.error('Authorization failures may be caused by having admin credentials from a previous CA instance.\n' +
-		'Try again after deleting the contents of the store directory '+store_path);
+		'Try again after deleting the contents of the store directory '+storePath);
 	}
 });
