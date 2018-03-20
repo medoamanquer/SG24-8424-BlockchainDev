707b568b72cb3279b3b65fd4822f4f9db0731757
Muhammad Salah <muhammad.salah1@ibm.com>
2018-03-20 17:56:05 +0200

    Restructure & convenience scripts

---
+++ environment/hlfv1/buildAndDeploy.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+source ./.env
+# indent text on echo
+function indent() {
+  c='s/^/       /'
+  case $(uname) in
+    Darwin) sed -l "$c";;
+    *)      sed -u "$c";;
+  esac
+}
+
+
+function printHelp ()
+{
+    printHeader
+    echo ""
+    echo -e "${RESET} options for this exec are: "
+    echo -e "${GREEN}-h ${RESET}Print this help information" | indent
+    echo -e "${GREEN}-n ${RESET}defaults to ${GREEN}voucher-network${RESET}. use ${YELLOW}-n your-named-network ${RESET}if you are using a different network name"  | indent
+    echo -e "\t\tyou will have to ensure that the name you use here is also the name you use in BOTH package.json files and in your application code" | indent
+    echo ""
+    echo ""
+}
+
+# displays where we are, uses the indent function (above) to indent each line
+function showStep ()
+    {
+        echo -e "${YELLOW}=====================================================" | indent
+        echo -e "${RESET}-----> $*" | indent
+        echo -e "${YELLOW}=====================================================${RESET}" | indent
+    }
+
+
+# print the header information for execution
+function printHeader ()
+{
+    echo ""
+    echo -e "${GREEN}This has been tested on Mac OSX thru High Sierra and Ubuntu V16 LTS" | indent
+    echo -e "${YELLOW}This script will create your Composer archive" | indent
+    echo ""
+}
+# get the command line options
+
+ while getopts "h:n:" opt; 
+do
+    case "$opt" in
+        h|\?)
+        printHelp
+        exit 0
+        ;;
+        n)  showStep "option passed for network name is: '$OPTARG'" 
+            if [[ $OPTARG != "" ]]; then 
+                NETWORK_NAME=$OPTARG 
+            fi
+        ;;
+    esac
+ done
+
+printHeader
+echo  "Parameters:"
+echo -e "Network Name is: ${GREEN} $NETWORK_NAME ${RESET}" | indent
+
+showStep "creating archive"
+./createArchive.sh -n $NETWORK_NAME
+showStep "starting network"
+./startup.sh
+showStep "deploying network"
+./deployNetwork.sh -n $NETWORK_NAME
