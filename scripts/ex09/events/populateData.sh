73ab84be287b565d7aa5810602268ddea9d41c7b
baldred <baldred@id.ibm.com>
2018-04-12 23:59:31 +0200

    Edited populateData script to insert more resources

--- scripts/ex09/events/populateData.sh
+++ scripts/ex09/events/populateData.sh
@@ -8,6 +8,9 @@ curl -X POST --header 'Content-Type: application/json' --header 'Accept: applica
 printf "\n\nCreating Person 3\n"
 curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{"$class":"org.acme.vehicle_network.Person","username": "anastasia","email": "anastasia%40email.com"}' 'http://localhost:3000/api/org.acme.vehicle_network.Person'
 
+printf "\n\nCreating Person 4\n"
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{"$class":"org.acme.vehicle_network.Person","username": "invi","email": "invi%40email.com"}' 'http://localhost:3000/api/org.acme.vehicle_network.Person'
+
 printf "\n\nCreating Manufacturer\n"
  curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{"$class": "org.acme.vehicle_network.Manufacturer", "companyId": "awesomecars",  
    "name": "Awesome Cars"}' 'http://localhost:3000/api/org.acme.vehicle_network.Manufacturer'
@@ -15,11 +18,14 @@ printf "\n\nCreating Manufacturer\n"
 printf "\n\nCreating Vehicle 1\n"
  curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ "$class": "org.acme.vehicle_network.Vehicle", "vin": "80KH", "trim": "Elegant","interior": "Suede", "extras": [],"make": "org.acme.vehicle_network.Manufacturer#awesomecars","modelType":"Primo","colour":"Black","yearOfManufacture": 2018}' 'http://localhost:3000/api/org.acme.vehicle_network.Vehicle'
 
- printf "\n\nCreating Vehicle 2\n"
+printf "\n\nCreating Vehicle 2\n"
  curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ "$class": "org.acme.vehicle_network.Vehicle", "vin": "81AB", "trim": "Economy","interior": "Plastic", "extras": [],"make": "org.acme.vehicle_network.Manufacturer#awesomecars","modelType":"Duramo","colour":"Yellow","yearOfManufacture": 2018}' 'http://localhost:3000/api/org.acme.vehicle_network.Vehicle'
 
- printf "\n\nCreating Vehicle 3\n"
+printf "\n\nCreating Vehicle 3\n"
  curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ "$class": "org.acme.vehicle_network.Vehicle", "vin": "71QR", "trim": "Military","interior": "Steel", "extras": [],"make": "org.acme.vehicle_network.Manufacturer#awesomecars","modelType":"Panther","colour":"Yellow","yearOfManufacture": 2018}' 'http://localhost:3000/api/org.acme.vehicle_network.Vehicle'
+
+printf "\n\nCreating Vehicle 4\n"
+ curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ "$class": "org.acme.vehicle_network.Vehicle", "vin": "89VP", "trim": "Luxury","interior": "Carbon", "extras": [],"make": "org.acme.vehicle_network.Manufacturer#awesomecars","modelType":"Luxor","colour":"Blue","yearOfManufacture": 2018}' 'http://localhost:3000/api/org.acme.vehicle_network.Vehicle'
 
 printf "\n\nCreating Order 1\n"
 curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ "$class": "org.acme.vehicle_network.Order", "orderId": "102", "orderStatus": "PLACED", "vehicle": "org.acme.vehicle_network.Vehicle#80KH", "owner": "org.acme.vehicle_network.Person#eunike" }' 'http://localhost:3000/api/org.acme.vehicle_network.Order'
@@ -29,5 +35,8 @@ curl -X POST --header 'Content-Type: application/json' --header 'Accept: applica
 
 printf "\n\nCreating Order 3\n"
 curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ "$class": "org.acme.vehicle_network.Order", "orderId": "104", "orderStatus": "PLACED", "vehicle": "org.acme.vehicle_network.Vehicle#71QR", "owner": "org.acme.vehicle_network.Person#anastasia" }' 'http://localhost:3000/api/org.acme.vehicle_network.Order'
+
+printf "\n\nCreating Order 4\n"
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ "$class": "org.acme.vehicle_network.Order", "orderId": "105", "orderStatus": "PLACED", "vehicle": "org.acme.vehicle_network.Vehicle#89VP", "owner": "org.acme.vehicle_network.Person#invi" }' 'http://localhost:3000/api/org.acme.vehicle_network.Order'
 
 printf "\n"
