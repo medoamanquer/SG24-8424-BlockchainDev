c716dfd63a30067fca2683007619265ce3423e42
baldred <baldred@id.ibm.com>
2018-04-14 11:38:18 +0200

    Modified scripts for Exercise 9

--- scripts/ex09/events/copyBnaFilesOnly.sh
+++ scripts/ex09/events/copyBnaFilesOnly.sh
@@ -5,11 +5,11 @@ rm ~/Blockchain_Redbook_Application/vehicle-manufacture-network/lib/script.js
 rm ~/Blockchain_Redbook_Application/vehicle-manufacture-network/models/org.acme.vehicle_network.cto
 rm ~/Blockchain_Redbook_Application/vehicle-manufacture-network/package.json
 
-cp ~/Blockchain_Redbook_Application/scripts/ex09/package.json.ex09 ~/Blockchain_Redbook_Application/vehicle-manufacture-network/
+cp ~/Blockchain_Redbook_Application/scripts/ex09/events/package.json.ex09 ~/Blockchain_Redbook_Application/vehicle-manufacture-network/
 mv ~/Blockchain_Redbook_Application/vehicle-manufacture-network/package.json.ex09 ~/Blockchain_Redbook_Application/vehicle-manufacture-network/package.json
 
-cp ~/Blockchain_Redbook_Application/scripts/ex09/script.js.ex09 ~/Blockchain_Redbook_Application/vehicle-manufacture-network/lib/
+cp ~/Blockchain_Redbook_Application/scripts/ex09/events/script.js.ex09 ~/Blockchain_Redbook_Application/vehicle-manufacture-network/lib/
 mv ~/Blockchain_Redbook_Application/vehicle-manufacture-network/lib/script.js.ex09 ~/Blockchain_Redbook_Application/vehicle-manufacture-network/lib/script.js
 
-cp ~/Blockchain_Redbook_Application/scripts/ex09/org.acme.vehicle_network.cto.ex09 ~/Blockchain_Redbook_Application/vehicle-manufacture-network/models/
+cp ~/Blockchain_Redbook_Application/scripts/ex09/events/org.acme.vehicle_network.cto.ex09 ~/Blockchain_Redbook_Application/vehicle-manufacture-network/models/
 mv ~/Blockchain_Redbook_Application/vehicle-manufacture-network/models/org.acme.vehicle_network.cto.ex09 ~/Blockchain_Redbook_Application/vehicle-manufacture-network/models/org.acme.vehicle_network.cto
