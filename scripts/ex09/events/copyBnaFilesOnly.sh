f6d3f0bed032bb994d007eee2c39cce77de1891d
baldred <baldred@id.ibm.com>
2018-04-14 10:32:52 +0200

    Added scripts for ex9 (1)

---
+++ scripts/ex09/events/copyBnaFilesOnly.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+
+
+rm ~/Blockchain_Redbook_Application/vehicle-manufacture-network/lib/script.js
+rm ~/Blockchain_Redbook_Application/vehicle-manufacture-network/models/org.acme.vehicle_network.cto
+rm ~/Blockchain_Redbook_Application/vehicle-manufacture-network/package.json
+
+cp ~/Blockchain_Redbook_Application/scripts/ex09/package.json.ex09 ~/Blockchain_Redbook_Application/vehicle-manufacture-network/
+mv ~/Blockchain_Redbook_Application/vehicle-manufacture-network/package.json.ex09 ~/Blockchain_Redbook_Application/vehicle-manufacture-network/package.json
+
+cp ~/Blockchain_Redbook_Application/scripts/ex09/script.js.ex09 ~/Blockchain_Redbook_Application/vehicle-manufacture-network/lib/
+mv ~/Blockchain_Redbook_Application/vehicle-manufacture-network/lib/script.js.ex09 ~/Blockchain_Redbook_Application/vehicle-manufacture-network/lib/script.js
+
+cp ~/Blockchain_Redbook_Application/scripts/ex09/org.acme.vehicle_network.cto.ex09 ~/Blockchain_Redbook_Application/vehicle-manufacture-network/models/
+mv ~/Blockchain_Redbook_Application/vehicle-manufacture-network/models/org.acme.vehicle_network.cto.ex09 ~/Blockchain_Redbook_Application/vehicle-manufacture-network/models/org.acme.vehicle_network.cto
---
+++ scripts/ex09/events/updateBnaEx09.sh
@@ -0,0 +1,28 @@
+#/bin/bash
+
+
+rm ~/Blockchain_Redbook_Application/vehicle-manufacture-network/lib/script.js
+rm ~/Blockchain_Redbook_Application/vehicle-manufacture-network/models/org.acme.vehicle_network.cto
+rm ~/Blockchain_Redbook_Application/vehicle-manufacture-network/package.json
+
+cp ~/Blockchain_Redbook_Application/scripts/ex09/package.json.ex09 ~/Blockchain_Redbook_Application/vehicle-manufacture-network/
+mv ~/Blockchain_Redbook_Application/vehicle-manufacture-network/package.json.ex09 ~/Blockchain_Redbook_Application/vehicle-manufacture-network/package.json
+
+cp ~/Blockchain_Redbook_Application/scripts/ex09/script.js.ex09 ~/Blockchain_Redbook_Application/vehicle-manufacture-network/lib/
+mv ~/Blockchain_Redbook_Application/vehicle-manufacture-network/lib/script.js.ex09 ~/Blockchain_Redbook_Application/vehicle-manufacture-network/lib/script.js
+
+cp ~/Blockchain_Redbook_Application/scripts/ex09/org.acme.vehicle_network.cto.ex09 ~/Blockchain_Redbook_Application/vehicle-manufacture-network/models/
+mv ~/Blockchain_Redbook_Application/vehicle-manufacture-network/models/org.acme.vehicle_network.cto.ex09 ~/Blockchain_Redbook_Application/vehicle-manufacture-network/models/org.acme.vehicle_network.cto
+
+cd ~/Blockchain_Redbook_Application/vehicle-manufacture-network/
+npm run prepublish
+
+printf "\nInstalling Vehicle Manufacture Business Network..\n"
+composer network install -a dist/vehicle-manufacture-network.bna -c PeerAdmin@hlfv1
+
+printf "\nUpgrade Vehicle Manufacture Business Network..\n"
+composer network upgrade -n vehicle-manufacture-network -V 0.2.5 -c PeerAdmin@hlfv1
+
+printf "\nPinging Vehicle Manufacture Business Network using Business Network Admin..\n"
+composer network ping --card admin@vehicle-manufacture-network
+
